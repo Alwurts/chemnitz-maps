008bbedc18e881cb8a2093a831fd9637e70cc5d0
Alwurts <jota780@gmail.com>
2024-07-12 00:13:38 +0200

    Remove tanstack router dev tools

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -2,14 +2,11 @@ import { Outlet, createRootRoute } from "@tanstack/react-router";
 
 import { AuthProvider } from "@/components/AuthProvider";
 
-import { TanStackRouterDevtools } from "@tanstack/router-devtools"
-
 export const Route = createRootRoute({
   component: () => (
     <>
       <AuthProvider>
         <Outlet />
-        <TanStackRouterDevtools />
       </AuthProvider>
     </>
   ),
